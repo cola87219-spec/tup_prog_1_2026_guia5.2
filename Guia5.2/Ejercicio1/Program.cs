915720c033ad42f3a59d168b488fbac9e36d9775
cola87219-spec <cola87219@gmail.com>
2026-04-30 09:45:17 -0300

    iniciando ejercicio_1

---
+++ Guia5.2/Ejercicio1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int promocion, nota_1, nota_2, nota_3, calculo_promedio;
+
+            Console.WriteLine("ingrese la condicion para promocioar");
+            promocion = Convert.ToInt32(Console.readline());
+            Console.WriteLine("ingrese las 3 notas del alumno");
+            nota_1 = Convert.ToInt32(Console.readline());
+            nota_
+
+        }
+    }
+}
